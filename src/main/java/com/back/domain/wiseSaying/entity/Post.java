080e1c0042db33e7f0c612bc4df630d58fa85c6e
CDH1126 <donghuyn1126@gmail.com>
2026-02-13 15:06:07 +0900

    refactor : (7,8) - WiseSaying -> Post 변경

--- src/main/java/com/back/domain/wiseSaying/entity/Post.java
+++ src/main/java/com/back/domain/wiseSaying/entity/Post.java
@@ -3,17 +3,18 @@ package com.back.domain.wiseSaying.entity;
 import jakarta.persistence.*;
 
 @Entity // 테이블과 연동하려면 사용해야 되는 어노테이션
-public class WiseSaying {
+public class Post {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     // 엔티티의 기본키(Primary Key) 값을 자동으로 생성해 주는 JPA 어노테이션
 
     @Column(length = 100, nullable = false)
-    private String content;
+    private String title;
 
-    @Column(length = 50, nullable = false)
-    private String author;
+    // TEXT 타입 사용 시 직접 명시를 해야 함
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String content;
 
 
 
