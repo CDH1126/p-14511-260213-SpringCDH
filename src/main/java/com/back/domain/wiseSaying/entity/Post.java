af896b81e1fe892826c8d19dc3a3a94345218a33
CDH1126 <donghuyn1126@gmail.com>
2026-02-23 11:20:20 +0900

    feat : (18) - Date 엔터티 추가

--- src/main/java/com/back/domain/wiseSaying/entity/Post.java
+++ src/main/java/com/back/domain/wiseSaying/entity/Post.java
@@ -3,6 +3,8 @@ package com.back.domain.wiseSaying.entity;
 import jakarta.persistence.*;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
+
 @Entity // 테이블과 연동하려면 사용해야 되는 어노테이션
 // @Entity 사용 시 생성자가 필수 (없을 경우 기본 생성자)
 @NoArgsConstructor
@@ -18,8 +20,13 @@ public class Post {
     // TEXT 타입 사용 시 직접 명시를 해야 함
     @Column(columnDefinition = "TEXT", nullable = false)
     private String content = "";
+    private LocalDateTime createDate;
+    private LocalDateTime modifyDate;
 
     public Post(String title, String content) {
+        this.createDate = LocalDateTime.now();
+        this.modifyDate = LocalDateTime.now();
+
         this.title = title;
         this.content = content;
     }
