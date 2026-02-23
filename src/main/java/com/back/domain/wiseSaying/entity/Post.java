56c89e6adbf2d0ab25e74319be27f3d857f31895
CDH1126 <donghuyn1126@gmail.com>
2026-02-23 11:07:33 +0900

    refactor : 생성자 부분 수정

--- src/main/java/com/back/domain/wiseSaying/entity/Post.java
+++ src/main/java/com/back/domain/wiseSaying/entity/Post.java
@@ -2,10 +2,11 @@ package com.back.domain.wiseSaying.entity;
 
 import jakarta.persistence.*;
 import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
 
 @Entity // 테이블과 연동하려면 사용해야 되는 어노테이션
 // @Entity 사용 시 생성자가 필수 (없을 경우 기본 생성자)
-@NoArgsConstructor
+@RequiredArgsConstructor // final 타입의 값에 대해서만
 public class Post {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -13,15 +14,15 @@ public class Post {
     // 엔티티의 기본키(Primary Key) 값을 자동으로 생성해 주는 JPA 어노테이션
 
     @Column(length = 100, nullable = false)
-    private String title;
+    private final String title;
 
     // TEXT 타입 사용 시 직접 명시를 해야 함
     @Column(columnDefinition = "TEXT", nullable = false)
-    private String content;
+    private final String content;
 
-    public Post(String title, String content) {
-        this.title = title;
-        this.content = content;
+    public Post() {
+        this.title = "";
+        this.content = "";
     }
 
 
