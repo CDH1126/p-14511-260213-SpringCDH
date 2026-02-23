2ebc9e9b46a5ab649e659c135f4e1a934f865a48
CDH1126 <donghuyn1126@gmail.com>
2026-02-23 15:57:41 +0900

    refactor : (34) - 엔터티 기본 속성을 BaseEntity 클래스로 생성 후 상

--- src/main/java/com/back/domain/post/entity/Post.java
+++ src/main/java/com/back/domain/post/entity/Post.java
@@ -1,43 +1,28 @@
 package com.back.domain.post.entity;
 
-import jakarta.persistence.*;
+import com.back.global.entity.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-
-import java.time.LocalDateTime;
-
 @Entity // 테이블과 연동하려면 사용해야 되는 어노테이션
 // @Entity 사용 시 생성자가 필수 (없을 경우 기본 생성자)
 @NoArgsConstructor
 @Setter
 @Getter
-@EntityListeners(AuditingEntityListener.class)
-public class Post {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
-    // 엔티티의 기본키(Primary Key) 값을 자동으로 생성해 주는 JPA 어노테이션
+public class Post extends BaseEntity {
 
     @Column(length = 100, nullable = false)
-    private String title = "";
+    private String title;
 
     // TEXT 타입 사용 시 직접 명시를 해야 함
     @Column(columnDefinition = "TEXT", nullable = false)
-    private String content = "";
+    private String content;
 
-    @CreatedDate // 데이터 생성 날짜 자동 생성
-    private LocalDateTime createDate;
-    @LastModifiedDate // 데이터 수정 시간 자동 반영
-    private LocalDateTime modifyDate;
+
 
     public Post(String title, String content) {
-        this.createDate = LocalDateTime.now();
-        this.modifyDate = LocalDateTime.now();
-
         this.title = title;
         this.content = content;
     }
---
+++ src/main/java/com/back/global/entity/BaseEntity.java
@@ -0,0 +1,27 @@
+package com.back.global.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@MappedSuperclass // 공통 필드를 여러 엔티티에서 상속받아 사용하기 위한 상속용 어노테이션
+@Getter
+@EntityListeners(AuditingEntityListener.class)
+public class BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    // 엔티티의 기본키(Primary Key) 값을 자동으로 생성해 주는 JPA 어노테이션
+
+    @CreatedDate // 데이터 생성 날짜 자동 생성
+    private LocalDateTime createDate;
+
+    @LastModifiedDate // 데이터 수정 시간 자동 반영
+    private LocalDateTime modifyDate;
+
+}
